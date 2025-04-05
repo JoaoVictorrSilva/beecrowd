9136489f972ddf5b29d9ebc2abe8eb64d77e277c
João Victor da Silva <joao.vt2807@gmail.com>
2025-04-05 08:18:11 -0300

    1004

---
+++ c++/beginner/bee_1004.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int x = 0;
+    int y = 0;
+
+    cin >> x;
+    cin >> y;
+    
+    cout << "PROD = " << x * y << endl;
+
+    return 0;
+}
