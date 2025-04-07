23fd49640e92714af9129f795a153a3eb7da1aac
João Victor da Silva <joao.vt2807@gmail.com>
2025-04-07 20:00:34 -0300

    1016

---
+++ c++/beginner/bee_1016.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main () {
+
+	int y = 0;
+	cin >> y;
+
+	cout << y * 2 << " minutos" << endl;
+
+	return 0;
+}
